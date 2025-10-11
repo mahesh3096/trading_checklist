449620b737b428e52408c590db3837be55d96173
mahesh3096 <buddybluddy@gmail.com>
2025-10-11 14:50:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,10 +41,11 @@ full_options_list = [
     "❤️ OPT DPoc,Strnd,BB, vwap(opt)",
     "❤️ OPT RSI,OI",
     "😎 I’m a risk manager. Max risk is 2 Lot",
-    """ 👍 Rev only @ interval not @ Rectangle👍 Before 3pm, ATR&ADX > T, Outside CPR, ORB, Rectangle entry<br>
-    👍 Strend&EMA20 Broken<br>
+    """ 👍 Rev only @ interval not @ Rectangle
+    👍 Before 3pm, ATR&ADX > T, Outside CPR, ORB, Rectangle entry
+    👍 Strend&EMA20 Broken
     👍 No pre BO & OBS (TL/IL)
-    👍 Ccandle closed & <40 BO Ilevel
+    👍 Color🕯️ closed & <40 BO Ilevel
     👍 Max 1,2 Tcandle before Ccandle """
 ]
 
@@ -350,6 +351,7 @@ elif page == "Settings ⚙️":
         risk_amount = capital * (risk_pct/100)
         lot_size = risk_amount // (stop_loss * quantity)
         st.success(f"Allowed Risk: ₹{risk_amount:.2f}, Lot Size: {int(lot_size)} lots")
+
 
 
 
